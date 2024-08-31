f48b7b9f56f13deb14d77b92435eae6b5cbbf303
miawithcode <cuttontail@gmail.com>
2024-08-31 19:10:52 +0800

    feat: add header

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Logo() {
+  return (
+    <Link href="/" className="font-bold uppercase text-zinc-900">
+      Evento
+    </Link>
+  );
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,30 @@
+import Link from 'next/link';
+import Logo from '../Logo';
+
+const routes = [
+  { href: '/', label: 'Home' },
+  { href: '/events/all', label: 'All Events' },
+];
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="border-b border-black/10">
+      <div className="mx-auto flex h-14 w-full max-w-screen-xl items-center justify-between px-2 md:px-6">
+        <Logo />
+
+        <nav>
+          <ul className="flex gap-x-4 text-sm md:gap-x-6">
+            {routes.map((route) => (
+              <li
+                key={route.href}
+                className="text-black/50 transition hover:text-black"
+              >
+                <Link href={route.href}>{route.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
